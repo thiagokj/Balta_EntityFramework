838286d85331744cb12aa961690ea6fdcd72668d
Thiago Cajaiba <thiago.cajaiba@gmail.com>
2023-02-16 06:23:03 -0300

    Adicionados examplos de CRUD

--- Program.cs
+++ Program.cs
@@ -34,9 +34,38 @@ using (var context = new BlogDataContext())
         O EF tem o retorno dos METADADOS e sabe quais registros devem 
         ser excluídos.
     */
-    var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
-    context.Remove(tag);
-    context.SaveChanges();
+    // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+    // context.Remove(tag);
+    // context.SaveChanges();
 
+    /*
+        Exemplo de SELECT no banco.
+        O retorno da consulta no banco só é efetivamente realizado quando
+        é feita uma chamada de um método (Ex: ToList()) após a atribuição
+        do contexto ou quando é feito um ForEach para percorrer os dados.
+    */
+    // var tags = context.Tags.ToList();
+    // foreach (var tag in tags)
+    // {
+    //     Console.WriteLine($"Tag: {tag.Name} - Slug: {tag.Slug}");
+    // }
+
+    /*
+        Exemplo de SELECT no banco, filtrando e ordenando.
+        É de EXTREMA importância fazer o filtro ANTES de chamar
+        o ToList(). Assim evitamos problemas de performance, pois serão
+        recuperados apenas os dados já filtrados.
+        Caso contrário, todos os registros são retornados e posteriormente
+        filtrados na aplicação.
+    */
+    var tags = context
+        .Tags
+        .Where(x => x.Name.Contains("DOT"))
+        .ToList();
+
+    foreach (var tag in tags)
+    {
+        Console.WriteLine($"Id: {tag.Id} - Tag: {tag.Name} - Slug: {tag.Slug}");
+    }
 }
 
